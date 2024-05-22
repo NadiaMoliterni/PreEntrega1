9e7c105e2850aee05eff18994c8913009cf1114f
nadiamoliterni <nanu.moli@hotmail.com>
2024-05-22 19:35:50 -0300

    server

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 
-import express from 'express';
-import cartRouter from './routes/cart.router.js'
-import productsRouter from './routes/products.router.js';
-import morgan from 'morgan';
-import { __dirname } from './routes/path.js';
-import { errorHandler } from './middlewares/errorHandler.js';
+// import express from 'express';
+// import cartRouter from './src/routes/cart.router.js'
+// import productsRouter from './src/routes/product.router.js';
+// import morgan from 'morgan';
+// import { __dirname } from './src/path.js';
+// import { errorHandler } from './src/middlewares/errorHandler.js';
 
-const app = express()
+// const app = express()
 
-app.use(express.static(__dirname + '/public'))
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(morgan('dev'))
+// app.use(express.static(__dirname + '/public'))
+// app.use(express.json())
+// app.use(express.urlencoded({extended:true}))
+// app.use(morgan('dev'))
 
-app.use('/api/carts', cartRouter);
-app.use('/api/products', productsRouter);
+// app.use('/api/carts', cartRouter);
+// app.use('/api/products', productsRouter);
 
-app.use(errorHandler);
-const PORT = 8080
+// app.use(errorHandler);
+// const PORT = 8080
 
-app.listen(PORT,()=>{
-    console.log(`Escuchando en el puerto ${PORT}`)
-})
+// app.listen(PORT,()=>{
+//     console.log(`Escuchando en el puerto ${PORT}`)
+// })
